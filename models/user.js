39e7c053acdcf37232f851bd403d7b65151de548
Goran <goranpiso@gmail.com>
2019-11-04 19:36:14 -0800

    fix: eslint issue

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const helper = require('../helper');
 
+// eslint-disable-next-line no-unused-vars
 const beforeSaveHook = async (user, options) => {
   if (user.password && user.changed('password')) {
     // eslint-disable-next-line no-param-reassign
