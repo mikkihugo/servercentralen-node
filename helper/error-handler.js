fce22f5585134387bc3857b3ceb8b5622ab43193
Goran <goranpiso@gmail.com>
2019-11-05 07:02:16 -0800

    refactor: catch error

--- helper/error-handler.js
+++ helper/error-handler.js
@@ -16,8 +16,7 @@ const errorHandler = (err, req, res, next) => {
     });
   }
   return res.status(500).json({
-    status: 500,
-    message: 'Service is unavailable',
+    error: err,
   });
 };
 
